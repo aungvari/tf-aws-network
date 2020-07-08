506fb2b6596fe8736372d8b0a219ea5fd7f3aade
aungvari <ungvariattila@live.com>
2020-07-08 21:31:54 +0200

    add vpc endpoint config

---
+++ vpc_endpoint_s3.tf
@@ -0,0 +1,15 @@
+# create VPC endpoint for internal s3 traffic
+data "aws_vpc_endpoint_service" "s3" {
+  service = "s3"
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id = aws_vpc.vpc-module.id
+  service_name = data.aws_vpc_endpoint_service.s3.service_name
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private_s3" {
+  count = length(var.private_subnets)
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  route_table_id  = aws_route_table.private-route.*.id[count.index]
+}
