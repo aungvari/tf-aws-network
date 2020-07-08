733b5aaf86ed61f9e6da9d439c985b9bd996c961
aungvari <ungvariattila@live.com>
2020-07-08 19:37:23 +0200

    add count to aws_eip

--- nat.tf
+++ nat.tf
@@ -1,11 +1,12 @@
 # NAT resources
 resource "aws_eip" "nat" {
+    count = length(var.private_subnets)
     vpc = true
 }
 
 resource "aws_nat_gateway" "nat-gw" {
     count = length(var.private_subnets)
-    allocation_id = aws_eip.nat.id
+    allocation_id = aws_eip.nat.*.id[count.index]
     subnet_id = aws_subnet.private-subnet.*.id[count.index]
     tags = {
     Name = "${var.name}-nat-gw-${count.index + 1}"
