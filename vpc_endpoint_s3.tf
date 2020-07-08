a27d5b7f7a5204c2ea1715a348115a4f8cb39537
aungvari <ungvariattila@live.com>
2020-07-08 21:36:01 +0200

    add vpc_endpoint_type for s3

--- vpc_endpoint_s3.tf
+++ vpc_endpoint_s3.tf
@@ -6,6 +6,7 @@ data "aws_vpc_endpoint_service" "s3" {
 resource "aws_vpc_endpoint" "s3" {
   vpc_id = aws_vpc.vpc-module.id
   service_name = data.aws_vpc_endpoint_service.s3.service_name
+  vpc_endpoint_type = "Gateway"
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private_s3" {
